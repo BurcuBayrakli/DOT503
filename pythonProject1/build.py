10194c4631c10958de63f53165c39fb31eabe687
Burcu <burcubayraklii@gmail.com>
2023-12-06 15:27:18 +1100

    Fix failed unit tests

--- pythonProject1/build.py
+++ pythonProject1/build.py
@@ -1,11 +1,38 @@
 import subprocess
+import sys
+import os
+#push
+# Define your project name and version
+project_name = "calculator"
+project_version = "1.0.0"
+#modifying
+# Define a function to run shell commands
+def run_command(command, cwd=None):
+    try:
+        subprocess.check_call(command, shell=True, cwd=cwd)
+    except subprocess.CalledProcessError as e:
+        sys.exit(e.returncode)
 
-def run_tests():
-    subprocess.run(["pytest"])
+# Clean previous build artifacts
+print("Cleaning previous build artifacts...")
+run_command("rm -rf build dist", cwd=os.path.dirname(__file__))
 
-def build_package():
-    subprocess.run(["python", "setup.py", "bdist_wheel"])
+# Run tests using pytest (modify the test command as needed)
+print("Running tests...")
+test_command = "pytest"
+run_command(test_command)
 
-if __name__ == "__main__":
-    run_tests()
-    build_package()
+# Build source distribution and wheel
+print("Building source distribution and wheel...")
+build_command = f"python setup.py sdist bdist_wheel"
+run_command(build_command)
+
+# Output distribution files to the 'dist' directory
+print("Distribution files are located in the 'dist' directory.")
+
+
+
+print("\nPackage build complete.")
+print(f"Your package name: {project_name}")
+print(f"Version: {project_version}")
+print("You can now distribute the package or executable as needed.")
---
+++ pythonProject1/pythonProject1/test_main.py
@@ -0,0 +1,32 @@
+import unittest
+from main import add_numbers
+
+class TestAddNumbers(unittest.TestCase):
+
+    def test_successful_addition(self):
+        result = add_numbers(2, 3)
+        self.assertEqual(result, 5)  # This test should pass
+
+    def test_failed_addition(self):
+        result = add_numbers(2, 3)
+        self.assertEqual(result, 6)  # This test should fail
+
+    def test_successful_subtraction(self):
+        result = add_numbers(5, 3)
+        self.assertEqual(result, 2)  # This test should pass
+
+    def test_failed_subtraction(self):
+        result = add_numbers(5, 3)
+        self.assertEqual(result, 8)  # This test should fail
+
+    def test_successful_addition(self):
+        result = add_numbers(2, 3)
+        self.assertEqual(result, 5)  # This test should pass
+
+    def test_another_successful_addition(self):
+        result = add_numbers(7, 8)
+        self.assertEqual(result, 15)  # This test should also pass
+
+
+if __name__ == '__main__':
+    unittest.main()
